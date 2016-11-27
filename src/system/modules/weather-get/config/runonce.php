96e57101a759d8c3d91259a986150c2a27ee96f8
Christian Najjar <info@christian-najjar.de>
2016-11-27 19:46:29 +0100

    update

--- src/system/modules/weather-get/config/runonce.php
+++ src/system/modules/weather-get/config/runonce.php
@@ -1,24 +1,14 @@
-<?php/**
- * inserttags extension for Contao Open Source CMS
- *
- * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
- * @author     terminal42 gmbh <info@terminal42.ch>
- * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
- * @link       http://github.com/terminal42/contao-inserttags
- */
+<?php
+
 class changeForecastToDarkSky extends Controller
 {
-	/**
-	 * Initialize the object
-	 */
+
 	public function __construct()
 	{
 		parent::__construct();
 		$this->import('Database');
 	}
-	/**
-	 * Execute all runonce files in module config directories
-	 */
+
 	public function run()
 	{
         if ($this->Database->tableExists('tl_weatherget_info'))
@@ -29,8 +19,7 @@ class changeForecastToDarkSky extends Controller
         }
 	}
 }
-/**
- * Instantiate controller
- */
+
+
 $objchangeForecastToDarkSky = new changeForecastToDarkSky();
 $objchangeForecastToDarkSky->run();
